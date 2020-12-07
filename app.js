b4ccf15fab436960253ff6043f4730eb6a1f0f41
ShyroVO <bosquet.laurie@outlook.com>
2020-12-07 16:52:28 +0100

    0.5

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let affichage = document.getElementById("affichage");
 let affichageResults = document.getElementById("results");
+let un = document.getElementById("un");
 
 let calcul = document.getElementById("calcul");
+
 
 
 // Effectuer le calcul:
