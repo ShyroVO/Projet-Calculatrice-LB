4bafada8d300b2c2ed3ef3167c5180beb8e7c4ef
ShyroVO <bosquet.laurie@outlook.com>
2020-12-07 16:33:58 +0100

    \o/

---
+++ app.js
@@ -0,0 +1,10 @@
+let affichage = document.getElementById("affichage");
+let affichageResults = document.getElementById("results");
+
+let calcul = document.getElementById("calcul");
+
+
+// Effectuer le calcul:
+calcul.addEventListener("click", function (){
+
+})
