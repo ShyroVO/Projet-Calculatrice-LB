3ce25116c8e2eb77cf333d455941ae0df2e24577
ShyroVO <bosquet.laurie@outlook.com>
2020-12-08 15:03:00 +0100

    2.0 // code 27 lignes / boucle 'for' avec 'switch'

--- app.js
+++ app.js
@@ -1,95 +1,27 @@
 let affichage = document.getElementById("affichage");
+let buttons = document.getElementsByTagName('button');
 
+for(let button of buttons) {
+    button.addEventListener('click', function () {
 
+        switch (button.id) {
+            case 'reset':
+                affichage.innerHTML = " ";
+                break;
 
-// Reset du calcul:
-document.getElementById("reset").addEventListener("click", function () {
-    affichage.innerHTML = " ";
-});
+            case 'egal':
+                let result = affichage.innerHTML;
+                affichage.innerHTML = eval(result);
+                break;
 
-// Ajouts chiffres:
-document.getElementById("0").addEventListener("click", function () {
-    affichage.innerHTML += 0;
-});
+            case 'copy':
+                let resultCopy = affichage.innerHTML;
+                navigator.clipboard.writeText(resultCopy);
+                break;
 
-document.getElementById("1").addEventListener("click", function () {
-    affichage.innerHTML += 1;
-});
+            default:
+                affichage.innerHTML += button.innerHTML;
 
-document.getElementById("2").addEventListener("click", function () {
-    affichage.innerHTML += 2;
-});
-
-document.getElementById("3").addEventListener("click", function () {
-    affichage.innerHTML += 3;
-});
-
-document.getElementById("4").addEventListener("click", function () {
-    affichage.innerHTML += 4;
-});
-
-document.getElementById("5").addEventListener("click", function () {
-    affichage.innerHTML += 5;
-});
-
-document.getElementById("6").addEventListener("click", function () {
-    affichage.innerHTML += 6;
-});
-
-document.getElementById("7").addEventListener("click", function () {
-    affichage.innerHTML += 7;
-});
-
-document.getElementById("8").addEventListener("click", function () {
-    affichage.innerHTML += 8;
-});
-
-document.getElementById("9").addEventListener("click", function () {
-    affichage.innerHTML += 9;
-});
-
-// Ajouts symbols d'opération:
-document.getElementById("parantheseG").addEventListener("click", function () {
-    affichage.innerHTML += "(";
-});
-
-document.getElementById("parantheseD").addEventListener("click", function () {
-    affichage.innerHTML += ")";
-});
-
-document.getElementById("plus").addEventListener("click", function () {
-    affichage.innerHTML += "+";
-});
-
-document.getElementById("moins").addEventListener("click", function () {
-    affichage.innerHTML += "-";
-});
-
-document.getElementById("multiplication").addEventListener("click", function () {
-    affichage.innerHTML += "*";
-});
-
-document.getElementById("division").addEventListener("click", function () {
-    affichage.innerHTML += "/";
-});
-
-document.getElementById("point").addEventListener("click", function () {
-    affichage.innerHTML += ".";
-});
-
-document.getElementById("modulo").addEventListener("click", function () {
-    affichage.innerHTML += "%";
-});
-
-// Effectuer le calcul:
-document.getElementById("egal").addEventListener("click", function () {
-    let result = affichage.innerHTML;
-    affichage.innerHTML = eval(result);
-
-});
-
-// Copier le résultat:
-document.getElementById("copy").addEventListener("click", function (){
-    let resultCopy = affichage.innerHTML;
-    navigator.clipboard.writeText(resultCopy);
-});
+        }
+    });
+}
